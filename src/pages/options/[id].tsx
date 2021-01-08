f82e1943df75c1ef5abe9593d43b3847dccd326c
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2021-01-09 00:45:26 +0900

    feat: arrow icon button

--- src/pages/options/[id].tsx
+++ src/pages/options/[id].tsx
@@ -9,9 +9,10 @@ import {
   List,
   ListItem,
   ListIcon,
+  IconButton,
   Stack,
+  Flex,
 } from '@chakra-ui/react'
-import { SunIcon } from '@chakra-ui/icons'
 import Author from '@components/Author'
 import baseURL from 'src/utils/baseURL'
 import OptionInteractor from 'src/interactors/options/OptionInteractor'
@@ -19,6 +20,7 @@ import { Option } from 'src/interactors/options/OptionMapper'
 import routes from 'routes'
 import Container from 'src/common/Container'
 import Panel from '@components/common/Panel'
+import Icon from '@components/common/Icon'
 import { iconMap } from '@components/common/Icon'
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -50,9 +52,15 @@ const OptionPage: NextPage<Props> = ({ option }) => {
       <Heading as="h1" size="xl">
         {data.title}
       </Heading>
-      <Box mt={2}>
+      <Flex justifyContent="space-between" mt={2}>
         <Author />
-      </Box>
+        <IconButton
+          aria-label="toggle expanded"
+          type="button"
+          variant="ghost"
+          icon={<Icon name={'ChevronDownIcon'} />}
+        />
+      </Flex>
       <Divider mt={4} mb={8} />
       <div>
         {data.options.map((option, i) => (
