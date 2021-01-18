60af5180c2ae366971dd822c0edf83ce1c158d03
JunkiYoshida <junkiyoshida@JunkiYoshidanoMacBook-Air.local>
2021-01-18 23:55:34 +0900

    feat: cut form component

---
+++ src/pages/options/new.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NextPage } from 'next'
+import { NextSeo } from 'next-seo'
+import OptionForm from '@components/optionForm/OptionForm'
+
+const NewPage: NextPage = () => {
+  return (
+    <>
+      <NextSeo title="新規共有 | Cime" />
+    </>
+  )
+}
+
+export default NewPage
